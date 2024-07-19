575cf5e4b2afdc27beb05678afa80106977acc0b
Webb Henderson <webb.henderson@gmail.com>
2024-07-18 22:52:05 -0600

    Only show images for specific user

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,17 @@
-import { db } from "../server/db";
+import { getMyImages } from "~/server/queries";
 
 export const dynamic = "force-dynamic";
 
-export default async function HomePage() {
-  const images = await db.query.images.findMany({
-    orderBy: (model, { desc }) => desc(model.id),
-  });
-
+async function Images() {
+  const images = await getMyImages();
   return (
-    <main className="">
-      <div className="flex flex-wrap gap-4">
-        {images.map((image) => (
-          <div key={image.id} className="flex w-48 flex-col">
-            <img src={image.url} />
-            <div>{image.name}</div>
-          </div>
-        ))}
-      </div>
-    </main>
+    <div className="flex flex-wrap gap-4">
+      {images.map((image) => (
+        <div key={image.id} className="flex w-48 flex-col">
+          <img src={image.url} />
+          <div>{image.name}</div>
+        </div>
+      ))}
+    </div>
   );
 }
